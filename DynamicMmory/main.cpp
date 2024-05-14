fb0448a4a435ff74eb34ba77d155f9a02531299f
Ngong_xj55@top-academy.ru <Ngong_xj55@top-academy.ru>
2024-05-14 20:50:00 +0300

    Split DONE

---
+++ DynamicMmory/main.cpp
@@ -0,0 +1,33 @@
+#include<iostream>
+using namespace std;
+#define tab "\t"
+void FillRand(int arr[], const int n);
+void Print(int arr[], const int n);
+void main()
+{
+	setlocale(LC_ALL, "");
+	int n = 5;
+	cout << "Enter array size: "; cin >> n;
+	int* arr = new int[n];
+	FillRand(arr, n);
+	Print(arr, n);
+	 
+	 
+	delete[] arr;
+}
+void FillRand(int arr[], const int n)
+{
+	for (int i = 0; i < n; i++)
+	{
+		*(arr + i) = rand() % 100;
+	}
+}
+void Print(int arr[], const int n)
+{
+	for (int i = 0; i < n; i++)
+	{
+		cout << arr[i] << tab;
+	}
+	cout << endl;
+}
+ 
