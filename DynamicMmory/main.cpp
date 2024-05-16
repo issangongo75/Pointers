b27c2dbe2584b1a59b8eb6b0ad316be27d174598
Ngong_xj55@top-academy.ru <Ngong_xj55@top-academy.ru>
2024-05-16 22:05:45 +0300

    push-back Done

--- DynamicMmory/main.cpp
+++ DynamicMmory/main.cpp
@@ -3,6 +3,8 @@ using namespace std;
 #define tab "\t"
 void FillRand(int arr[], const int n);
 void Print(int arr[], const int n);
+int* push_back(int arr[], int& n,const int value);
+
 void main()
 {
 	setlocale(LC_ALL, "");
@@ -11,6 +13,23 @@ void main()
 	int* arr = new int[n];
 	FillRand(arr, n);
 	Print(arr, n);
+	int value;
+	cout << "Enter a value to be added: "; cin >> value;
+	arr = push_back(arr, n, value);
+	Print(arr, n);
+	/*int* buffer = new int[n + 1];
+	for (int i = 0; i < n; i++)
+	{
+		buffer[i] = arr[i];
+	}
+	delete[] arr;
+	int a = 2;
+	int b = 3;
+	a = b;
+	arr = buffer;
+	arr[n] = value;
+	n++;
+	Print(arr, n);*/
 	 
 	 
 	delete[] arr;
@@ -29,5 +48,21 @@ void Print(int arr[], const int n)
 		cout << arr[i] << tab;
 	}
 	cout << endl;
+}
+int* push_back(int arr[], int& n,const int value)
+{
+	int* buffer = new int[n + 1];
+	for (int i = 0; i < n; i++)
+	{
+		buffer[i] = arr[i];
+	}
+	delete[] arr;
+	int a = 2;
+	int b = 3;
+	a = b;
+	arr = buffer;
+	arr[n] = value;
+	n++;
+	return arr;
 }
  
