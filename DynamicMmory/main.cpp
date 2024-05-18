073e76080b817ccf78fffa4d945495c83b510bc7
Ngong_xj55@top-academy.ru <Ngong_xj55@top-academy.ru>
2024-05-18 20:10:32 +0300

    Single dimentional dynamic arrays done

--- DynamicMmory/main.cpp
+++ DynamicMmory/main.cpp
@@ -4,6 +4,11 @@ using namespace std;
 void FillRand(int arr[], const int n);
 void Print(int arr[], const int n);
 int* push_back(int arr[], int& n,const int value);
+int* push_front(int arr[], int& n, const int value);
+int* pop_back(int arr[], int& n);
+int* pop_front(int arr[], int& n);
+int* erase(int arr[], int& n, const int index);
+int* insert(int arr[], int& n, const int value, const int index);
 
 void main()
 {
@@ -17,11 +22,28 @@ void main()
 	cout << "Enter a value to be added: "; cin >> value;
 	arr = push_back(arr, n, value);
 	Print(arr, n);
+	cout << "Enter a value to be added: "; cin >> value;
+	arr = push_front(arr, n, value);
+	Print(arr, n);
+	arr = pop_back(arr, n);
+	Print(arr, n);
+	arr = pop_front(arr, n);
+	Print(arr, n);
+	int index;
+	cout << "Enter a index to be added: "; cin >> index;
+	cout << "Enter a value to be added: "; cin >> value;
+	arr = insert(arr, n, value, index);
+	Print(arr, n);
+	cout << "Enter a index to be added: "; cin >> index;
+	arr = erase(arr, n, index);
+	Print(arr, n);
+	delete[]arr;
 	/*int* buffer = new int[n + 1];
 	for (int i = 0; i < n; i++)
 	{
 		buffer[i] = arr[i];
 	}
+
 	delete[] arr;
 	int a = 2;
 	int b = 3;
@@ -65,4 +87,55 @@ int* push_back(int arr[], int& n,const int value)
 	n++;
 	return arr;
 }
- 
+int* push_front(int arr[], int& n, const int value)
+{
+	int* buffer = new int[n + 1];
+	for (int i = 0; i < n; i++)buffer[i + 1] = arr[i];
+	delete[] arr;
+	buffer[0] = value;
+	n++;
+	return buffer;
+}
+int* insert(int arr[], int& n, const int value, const int index)
+{
+	int* buffer = new int[n + 1];
+	//for (int i = 0; i < index; i++)buffer[i] = arr[i];
+	//for (int i = index; i < n; i++)buffer[i+1] = arr[i];
+	for (int i = 0; i < n; i++)
+	{
+		//if (i < index)buffer[i] = arr[i];
+		//else buffer[i + 1] = arr[i];
+		//i < index ? buffer[i] = arr[i] : buffer[i + 1] = arr[i];
+		buffer[i < index ? i : i + 1] = arr[i];
+	}
+	delete[]arr;
+	buffer[index] = value;
+	n++;
+	return buffer;
+}
+int* pop_back(int arr[], int& n)
+{
+	int* buffer = new int[--n];
+	for (int i = 0; i < n; i++)buffer[i] = arr[i];
+	delete[]arr;
+	return buffer;
+}
+int* pop_front(int arr[], int& n)
+{
+	int* buffer = new int[--n];
+	for (int i = 0; i < n; i++)buffer[i] = arr[i + 1];
+	delete[]arr;
+	return buffer;
+}
+int* erase(int arr[], int& n, const int index)
+{
+	int* buffer = new int[--n];
+	//for (int i = 0; i < index; i++)buffer[i] = arr[i];
+	//for (int i = index; i < n; i++)buffer[i] = arr[i + 1];
+	for (int i = 0; i < n; i++)
+	{
+		buffer[i] = arr[i < index ? i : i + 1];
+	}
+	delete[] arr;
+	return buffer;
+}
